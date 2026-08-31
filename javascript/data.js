a3bcc293ba3bb30c4e5c7ec0d1844ce951c887e8
Fayazi Ai-Network <122686359+Fayazic8@users.noreply.github.com>
2026-09-01 05:53:48 +0700

    update javascript/data.js

--- javascript/data.js
+++ javascript/data.js
@@ -183,8 +183,40 @@ const dataSpjHarian = {
         { nama: "Anggur Hijau",              berat: 50,  energi: 15.0,   prot: 0.25,  lemak: 0.10,   karb: 3.40,  serat: 0.60 }
       ]
     }
-  }
+  },
 
+  "2026-09-01": {
+    reguler: {
+      sekolah: [
+        { nama: "TK Al Anwar", kecil: 45, besar: 0 },
+        { nama: "TK Annuqayah", kecil: 77, besar: 0 },
+        { nama: "MI 1 Annuqayah", kecil: 44, besar: 74 },
+        { nama: "MI 3 Annuqayah", kecil: 65, besar: 64 },
+        { nama: "MTs 1 Annuqayah", kecil: 0, besar: 838 },
+        { nama: "MTs 2 Annuqayah", kecil: 0, besar: 100 },
+        { nama: "MTs 3 Annuqayah", kecil: 0, besar: 97 },
+        { nama: "MA 1 Annuqayah", kecil: 0, besar: 430 },
+        { nama: "MA 2 Annuqayah", kecil: 0, besar: 98 },
+        { nama: "MA Tahfidh", kecil: 0, besar: 210 },
+        { nama: "SMA Annuqayah", kecil: 0, besar: 499 },
+        { nama: "SMA 3 Annuqayah", kecil: 0, besar: 149 }
+      ],
+      targetKecil: 413, targetBesar: 708,
+      fotoKecil: "https://lh3.googleusercontent.com/d/1ya8F1ywTGa80TSbri8_X2d2l-2H-Tcgm",
+      fotoBesar: "https://lh3.googleusercontent.com/d/1uWv8dP5s_i2TXs2LgkyZzROQBfdmre-K",
+      menuKecil: [
+        { nama: "Nasi Putih",              berat: 125, energi: 225.0,  prot: 3.75,  lemak: 0.375, karb: 49.75, serat: 0.25 },
+        { nama: "Telur Dadar Saus Padang", berat: 68,  energi: 154.71, prot: 7.12,  lemak: 10.43, karb: 1.275, serat: 0.00 },
+        { nama: "Tahu Goreng",             berat: 60,  energi: 88.2,   prot: 5.99,  lemak: 5.11,  karb: 0.44,  serat: 0.04 },
+        { nama: "Sayur Bening",            berat: 27,  energi: 48.08,  prot: 0.186, lemak: 5.064, karb: 0.782, serat: 0.226 },
+        { nama: "Susu",                    berat: 125, energi: 76.25,  prot: 4.00,  lemak: 4.375, karb: 5.375, serat: 0.00 },
+        { nama: "Kelengkeng",              berat: 20,  energi: 30.0,   prot: 0.66,  lemak: 0.05,  karb: 7.57,  serat: 0.00 }
+      ],
+      menuBesar: [
+        // Rincian menu besar akan kita susun di langkah berikutnya
+      ]
+    }
+  }
 
 
   
